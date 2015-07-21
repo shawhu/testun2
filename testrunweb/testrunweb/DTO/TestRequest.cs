256899f68cbe1f013977fd7236567e5520ba3606
Harry Xiao <shawhu@gmail.com>
2015-07-21 16:37:28 +0800

    创建了第一个servicestack的服务

---
+++ testrunweb/testrunweb/DTO/TestRequest.cs
@@ -0,0 +1,15 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testrunweb
+{
+    [Route("/test")]
+    public class TestRequest
+    {
+        public int member_id { get; set; }
+        public string full_name { get; set; }
+    }
+}
---
+++ testrunweb/testrunweb/Main_Services.cs
@@ -0,0 +1,20 @@
+﻿using ServiceStack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace testrunweb
+{
+    [EnableCors(allowedMethods: "GET, POST, PUT, DELETE, OPTIONS", allowedHeaders: "Accept,Content-Type,Authorization")]
+    public class Main_Services : Service
+    {
+        //test
+        public int Get(TestRequest req)
+        {
+
+            return req.full_name.Length;
+               
+        }
+    }
+}
